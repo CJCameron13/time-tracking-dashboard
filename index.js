a0761132d13c63b746cf1db8d2c9d85b246e4fb1
CJ Cameron <96845401+CJCameron13@users.noreply.github.com>
2024-09-05 13:38:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,99 @@
 const daily = document.querySelector('#daily')
 const weekly = document.querySelector('#weekly')
 const monthly = document.querySelector('#monthly')
+const dailyTime = document.querySelectorAll('.daily-time')
+const weeklyTime = document.querySelectorAll('.weekly-time')
+const monthlyTime = document.querySelectorAll('.monthly-time')
+const yesterday = document.querySelectorAll('.yesterday')
+const lastWeek = document.querySelectorAll('.last-week')
+const lastMonth = document.querySelectorAll('.last-month')
+
+daily.addEventListener('click', () => {
+    daily.classList.add('active')
+    weekly.classList.remove('active')
+    monthly.classList.remove('active')
+
+    dailyTime.forEach((time) => {
+        time.classList.remove('hidden')
+    })
+
+    weeklyTime.forEach((time) => {
+        time.classList.add('hidden')
+    })
+
+    monthlyTime.forEach((time) => {
+        time.classList.add('hidden')
+    })
+
+    yesterday.forEach((previous) => {
+        previous.classList.remove('hidden')
+    })
+
+    lastWeek.forEach((previous) => {
+        previous.classList.add('hidden')
+    })
+
+    lastMonth.forEach((previous) => {
+        previous.classList.add('hidden')
+    })
+})
+
+weekly.addEventListener('click', () => {
+    daily.classList.remove('active')
+    weekly.classList.add('active')
+    monthly.classList.remove('active')
+
+    dailyTime.forEach((time) => {
+        time.classList.add('hidden')
+    })
+
+    weeklyTime.forEach((time) => {
+        time.classList.remove('hidden')
+    })
+
+    monthlyTime.forEach((time) => {
+        time.classList.add('hidden')
+    })
+
+    yesterday.forEach((previous) => {
+        previous.classList.add('hidden')
+    })
+
+    lastWeek.forEach((previous) => {
+        previous.classList.remove('hidden')
+    })
+
+    lastMonth.forEach((previous) => {
+        previous.classList.add('hidden')
+    })
+})
+
+monthly.addEventListener('click', () => {
+    daily.classList.remove('active')
+    weekly.classList.remove('active')
+    monthly.classList.add('active')
+
+    dailyTime.forEach((time) => {
+        time.classList.add('hidden')
+    })
+
+    weeklyTime.forEach((time) => {
+        time.classList.add('hidden')
+    })
+
+    monthlyTime.forEach((time) => {
+        time.classList.remove('hidden')
+    })
+
+    yesterday.forEach((previous) => {
+        previous.classList.add('hidden')
+    })
+
+    lastWeek.forEach((previous) => {
+        previous.classList.add('hidden')
+    })
+
+    lastMonth.forEach((previous) => {
+        previous.classList.remove('hidden')
+    })
+})
